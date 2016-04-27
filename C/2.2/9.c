62a515e577d588af0837e0d8f741e1c0ee00104d
Николай <polovnev002@yandex.ru>
2016-04-28 00:34:41 +0300

    hello world

--- C/2.2/9.c
+++ C/2.2/9.c
@@ -4,20 +4,25 @@
 
 int main ()
  {
-   int i=0;
-   float sred,kol;
+   int i=0,k=0;
+   float sred,kol,n;
    float sum=0;
-   char chi[500];
+   char chi[500],ch[500];
    printf("Введите число \n");
    scanf ("%s",chi);
    kol=strlen(chi);
-  
+   n=kol;
 
- while (i<kol)
-   { chi[i]=chi[i]-48;//в число 1 , к примеру , записывается , не как 1 , а как 49
+ while (i<n)
+ 
+   {if (chi[i]!=48) {k=k+1;}
+  chi[i]=chi[i]-48;//в число 1 , к примеру , записывается , не как 1 , а как 49
+
 sum=sum+chi[i];
-     i=i+1;   }
-  sred=sum/kol;
+     i=i+1;
+      }
+
+  sred=sum/k;
   printf ("Количество цифр в числе = %.0f , сумма всех цифр =%.2f , сренее значение =%.3f \n",kol , sum , sred);
   
   
